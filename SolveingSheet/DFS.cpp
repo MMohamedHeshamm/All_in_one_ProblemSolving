8f4a3497d887da3451dcb923c0cabd39996c57e5
adham.baker.74@gmail.com <adham.baker.74@gmail.com>
2026-02-15 11:10:28 +0200

    A

--- SolveingSheet/DFS.cpp
+++ SolveingSheet/DFS.cpp
@@ -27,24 +27,24 @@ public:
 
 };
 
-
-int main() 
- {
-    int n = 5;
- 
-    vector<vector<int>> adj(n);
- 
-	  DFS DFSObj;
-
-    // Undirected Graph
-    adj[0] = { 1, 2 };
-    adj[1] = { 0, 3 };
-    adj[2] = { 0, 4 };
-    adj[3] = { 1 };
-    adj[4] = { 2 };
-
-    vector<bool> visited(n, false);
-
-    cout << "DFS : ";
-    DFSObj.dfs(0, adj, visited);
-}
+//
+//int main() 
+// {
+//    int n = 5;
+// 
+//    vector<vector<int>> adj(n);
+// 
+//	  DFS DFSObj;
+//
+//    // Undirected Graph
+//    adj[0] = { 1, 2 };
+//    adj[1] = { 0, 3 };
+//    adj[2] = { 0, 4 };
+//    adj[3] = { 1 };
+//    adj[4] = { 2 };
+//
+//    vector<bool> visited(n, false);
+//
+//    cout << "DFS : ";
+//    DFSObj.dfs(0, adj, visited);
+//}
--- SolveingSheet/Main.cpp
+++ SolveingSheet/Main.cpp
@@ -19,7 +19,20 @@ using namespace std;
 int main()
 {
 
+	int n = 7;
+		BFS bfsObj;
+		vector<vector<int>> adj(n);
 	
-
+		adj[0] = { 1, 2 };
+		adj[1] = { 0, 3 , 6 };
+		adj[2] = { 0, 4 , 5 };
+		adj[3] = { 1 };
+		adj[4] = { 2 };
+		adj[5] = { 2 };
+		adj[6] = { 1 };
+	
+		cout << "BFS : ";
+		bfsObj.bfs(0, adj, n);
+	
 
 }
