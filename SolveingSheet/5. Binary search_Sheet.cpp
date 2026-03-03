a4ddb61421e36ce62dd50a5084f7126c64b2b054
adham.baker.74@gmail.com <adham.baker.74@gmail.com>
2026-03-03 12:31:36 +0200

    divide and conquer

--- SolveingSheet/5. Binary search_Sheet.cpp
+++ SolveingSheet/5. Binary search_Sheet.cpp
@@ -29,3 +29,35 @@ int main() {
 
     return 0;
 }
+
+
+//-----------------------------------------
+
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int binarySearch(vector<int>& nums, int left, int right, int target) {
+
+    if (left > right)
+        return left;
+
+    int mid = left + (right - left) / 2;
+
+    if (nums[mid] == target)
+        return mid;
+    else if (nums[mid] < target)
+        return binarySearch(nums, mid + 1, right, target);
+    else
+        return binarySearch(nums, left, mid - 1, target);
+}
+
+int searchInsert(vector<int>& nums, int target) {
+    return binarySearch(nums, 0, nums.size() - 1, target);
+}
+
+int main() {
+    vector<int> nums = { 1,3,5,6 };
+    cout << searchInsert(nums, 2);
+}
